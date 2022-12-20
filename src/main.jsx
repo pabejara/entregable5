3932bce7e81262844487c9dfbc67378f66f0528d
pabejara <pablobejarano@gmail.com>
2022-12-20 01:34:52 -0500

    entregable 5

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { HashRouter } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+// Redux
+import { Provider } from 'react-redux'
+import store from './store'
+// react-router
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter>
+  </React.StrictMode>,
+)
+
---
+++ src/pages/PokedexInfo.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import './styles/pokeInfo.css'
+
+const PokedexInfo = () => {
+
+const {id} = useParams()
+
+const [pokemon, setPokemon] = useState()
+
+useEffect(() => {
+  const URL = `https://pokeapi.co/api/v2/pokemon/${id}`
+  axios.get(URL)
+    .then(res => setPokemon(res.data))
+    .catch(err => console.log(err))
+}, [id])
+
+console.log(pokemon)
+
+  return (
+    <div className='poke__info' >
+      <img src={pokemon?.sprites.other['official-artwork'].front_default} alt="" />
+    </div>
+  )
+}
+
+export default PokedexInfo
